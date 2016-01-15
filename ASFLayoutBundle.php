7e64370d4cbedee847ab87a161df16944fd6ff2a
Artscore Studio <info@artscore-studio.fr>
2016-01-15 15:59:59 +0100

    Create ASFLayoutBundle class

---
+++ ASFLayoutBundle.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the Artscore Studio Framework package.
+ *
+ * (c) 2012-2015 Nicolas Claverie <info@artscore-studio.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace ASF\LayoutBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Layout Bundle
+ * 
+ * @author Nicolas Claverie <info@artscore-studio.fr>
+ *
+ */
+class ASFLayoutBundle extends Bundle {}
