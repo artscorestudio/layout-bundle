80cf80bf7f7c89fca74d125953b91ea654dd62e6
Nicolas Claverie <info@artscore-studio.fr>
2016-02-02 17:00:18 +0100

    [dev] commands and scripthandler

---
+++ Command/CopyLessFilesCommand.php
@@ -0,0 +1,151 @@
+<?php
+/*
+ * This file is part of the Artscore Studio Framework package.
+ *
+ * (c) Nicolas Claverie <info@artscore-studio.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace ASF\LayoutBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOException;
+use Symfony\Component\Console\Command\Command;
+
+/**
+ * Copy Less Files Command
+ * 
+ * @author Nicolas Claverie <info@artscore-studio.fr>
+ *
+ */
+class CopyLessFilesCommand extends ContainerAwareCommand
+{
+    /**
+     * @var array
+     */
+    protected $twbs_config;
+
+    /**
+     * {@inheritDoc}
+     * @see \Symfony\Component\Console\Command\Command::configure()
+     */
+    protected function configure()
+    {
+        $this->setName('asf:twbs:less:copy')
+            ->setDescription('Install icons ansd copy principal Twitter Bootstrap files (bootstrap.less, theme.less and variables.less)');
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see \Symfony\Component\Console\Command\Command::execute()
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->twbs_config = $this->getContainer()->getParameter('asf_layout.supported_assets')['twbs'];
+        
+        if ( !isset($this->twbs_config['customize']['less']['dest_dir']) ) {
+            $output->writeln(sprintf('<error>Please check bundle\'s documentation for customize Twitter Bootstrap less files.</error>'));
+            return;
+        }
+        
+        $dest_dir = $this->twbs_config['customize']['less']['dest_dir'];
+        $src_dir = sprintf('%s/%s', $this->twbs_config['assets_dir'], 'less');
+        
+        $finder = new Finder();
+        $fs = new Filesystem();
+        
+        try {
+            if ( !$fs->exists($dest_dir) )
+                $fs->mkdir($dest_dir);
+        
+        } catch (IOException $e) {
+            $output->writeln(sprintf('<error>Could not create directory %s.</error>', $dest_dir));
+            return;
+        }
+        
+        if (false === file_exists($src_dir)) {
+            $output->writeln(sprintf(
+                '<error>Fonts directory "%s" does not exist. Did you install Twitter Bootstrap ? '.
+                'If you used something other than Composer you need to manually change the path in '.
+                '"asf.twbs.assets_dir".</error>',
+                $src_dir
+                ));
+            return;
+        }
+        
+        $finder->files()->in($src_dir);
+        $path = $this->generateRelativePath($dest_dir, $src_dir);
+
+        foreach ($finder as $file) {
+            $copy = false;
+            if ( !isset($this->twbs_config['customize']['less']['files']) ) {
+                $copy = true;
+            } elseif ( in_array($file->getBaseName(), $this->twbs_config['customize']['less']['files']) ) {
+                $copy = true;
+            }
+            
+            $dest = sprintf('%s/%s', $dest_dir, $file->getBaseName());
+            
+            if ( $copy == true && !$fs->exists($dest)) {
+                try {
+                    $fs->copy($file, $dest);
+                } catch (IOException $e) {
+                    $output->writeln(sprintf('<error>Could not copy %s</error>', $file->getBaseName()));
+                    return;
+                }
+            }
+        }
+
+        $output->writeln(sprintf('Copied less files to <comment>%s</comment>.', $dest_dir));
+    }
+    
+    /**
+     * Returns the relative path $from to $to.
+     * 
+     * @see https://github.com/braincrafted/bootstrap-bundle/blob/develop/Util/PathUtil.php
+     * 
+     * @param string $from
+     * @param string $to
+     * 
+     * @return string
+     */
+    public function generateRelativePath($from, $to)
+    {
+        // some compatibility fixes for Windows paths
+        $from = is_dir($from) ? rtrim($from, '\/') . '/' : $from;
+        $to   = is_dir($to)   ? rtrim($to, '\/') . '/'   : $to;
+        $from = str_replace('\\', '/', $from);
+        $to   = str_replace('\\', '/', $to);
+        
+        $from     = explode('/', $from);
+        $to       = explode('/', $to);
+        $relPath  = $to;
+        var_dump($from);
+        foreach ($from as $depth => $dir) {
+            // find first non-matching dir
+            if ($dir === $to[$depth]) {
+                // ignore this directory
+                array_shift($relPath);
+            } else {
+                // get number of remaining dirs to $from
+                $remaining = count($from) - $depth;
+                if ($remaining > 1) {
+                    // add traversals up to first matching dir
+                    $padLength = (count($relPath) + $remaining - 1) * -1;
+                    $relPath = array_pad($relPath, $padLength, '..');
+                    break;
+                } else {
+                    $relPath[0] = './' . $relPath[0];
+                }
+            }
+        }
+        
+        return implode('/', $relPath);
+    }
+}
--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -29,28 +29,53 @@ class ScriptHandler
     protected static $options = array(
         'symfony-app-dir' => 'app',
         'symfony-web-dir' => 'web',
-        'symfony-bin-dir' => 'bin',
-        'symfony-assets-install' => 'hard'
+        'symfony-assets-install' => 'hard',
+        'symfony-cache-warmup' => false,
     );
+    
+    /**
+     * Asks if the new directory structure should be used, installs the structure if needed.
+     *
+     * @param CommandEvent $event
+     */
+    public static function defineDirectoryStructure(CommandEvent $event)
+    {
+        $options = static::getOptions($event);
+    
+        if (!getenv('SENSIOLABS_ENABLE_NEW_DIRECTORY_STRUCTURE') || !$event->getIO()->askConfirmation('Would you like to use Symfony 3 directory structure? [y/N] ', false)) {
+            return;
+        }
+    
+        $rootDir = getcwd();
+        $appDir = $options['symfony-app-dir'];
+        $webDir = $options['symfony-web-dir'];
+        $binDir = static::$options['symfony-bin-dir'] = 'bin';
+        $varDir = static::$options['symfony-var-dir'] = 'var';
+    }
     
     public static function install(CommandEnvent $event)
     {
-        $options = self::getOptions($event);
-        $consolePathOptionsKey = array_key_exists('symfony-bin-dir', $options) ? 'symfony-bin-dir' : 'symfony-app-dir';
-        $consolePath = $options[$consolePathOptionsKey];
+        $options = static::getOptions($event);
+        $consoleDir = static::getConsoleDir($event, 'install bootstrap fonts and files');
         
-        if (!is_dir($consolePath)) {
-            printf(
-                'The %s (%s) specified in composer.json was not found in %s, can not build bootstrap file.%s',
-                $consolePathOptionsKey,
-                $consolePath,
-                getcwd(),
-                PHP_EOL
-                );
+        if (null === $consoleDir) {
             return;
         }
         
-        static::executeCommand($event, $consolePath, 'asf:bootstrap:install', $options['process-timeout']);
+        $webDir = $options['symfony-web-dir'];
+        
+        $symlink = '';
+        if ($options['symfony-assets-install'] == 'symlink') {
+            $symlink = '--symlink ';
+        } elseif ($options['symfony-assets-install'] == 'relative') {
+            $symlink = '--symlink --relative ';
+        }
+        
+        if (!static::hasDirectory($event, 'symfony-web-dir', $webDir, 'install bootstrap fonts and files')) {
+            return;
+        }
+        
+        static::executeCommand($event, $consoleDir, 'asf:bootstrap:install '.$symlink.escapeshellarg($webDir), $options['process-timeout']);
     }
     
     /**
@@ -62,36 +87,64 @@ class ScriptHandler
      */
     public static function executeCommand(CommandEvent $event, $consolePath, $cmd, $timeout = 300)
     {
-        $php = escapeshellarg(self::getPhp(false));
-        $console = escapeshellarg($consolePath.'/console');
-        if ( $event->getID()->isDecorated() )
+        $php = escapeshellarg(static::getPhp(false));
+        $phpArgs = implode(' ', array_map('escapeshellarg', static::getPhpArguments()));
+        $console = escapeshellarg($consoleDir.'/console');
+        if ($event->getIO()->isDecorated()) {
             $console .= ' --ansi';
-        
-        $process = new Process($php.' '.$console.' '.$cmd, null, null, null, $timeout);
-        $process->run(function($type, $buffer) {
-            echo $buffer;
-        });
-        
-        if ( !$process->isSuccessful() ) {
-            throw new \RuntimeException(sprintf('An error occured when executing the "%s" command.', escapeshellarg($cmd)));
         }
+
+        $process = new Process($php.($phpArgs ? ' '.$phpArgs : '').' '.$console.' '.$cmd, null, null, null, $timeout);
+        $process->run(function ($type, $buffer) use ($event) { $event->getIO()->write($buffer, false); });
+        if (!$process->isSuccessful()) {
+            throw new \RuntimeException(sprintf("An error occurred when executing the \"%s\" command:\n\n%s\n\n%s.", escapeshellarg($cmd), $process->getOutput(), $process->getErrorOutput()));
+        }
+    }
+    
+    protected static function hasDirectory(CommandEvent $event, $configName, $path, $actionName)
+    {
+        if (!is_dir($path)) {
+            $event->getIO()->write(sprintf('The %s (%s) specified in composer.json was not found in %s, can not %s.', $configName, $path, getcwd(), $actionName));
+    
+            return false;
+        }
+    
+        return true;
+    }
+    
+    /**
+     * Clears the Symfony cache.
+     *
+     * @param $event CommandEvent A instance
+     */
+    public static function clearCache(CommandEvent $event)
+    {
+        $options = static::getOptions($event);
+        $consoleDir = static::getConsoleDir($event, 'clear the cache');
+    
+        if (null === $consoleDir) {
+            return;
+        }
+    
+        $warmup = '';
+        if (!$options['symfony-cache-warmup']) {
+            $warmup = ' --no-warmup';
+        }
+    
+        static::executeCommand($event, $consoleDir, 'cache:clear'.$warmup, $options['process-timeout']);
     }
     
     protected static function getOptions(CommandEvent $event)
     {
         $options = array_merge(static::$options, $event->getComposer()->getPackage()->getExtra());
-    
+
         $options['symfony-assets-install'] = getenv('SYMFONY_ASSETS_INSTALL') ?: $options['symfony-assets-install'];
-    
+
         $options['process-timeout'] = $event->getComposer()->getConfig()->get('process-timeout');
-    
+
         return $options;
     }
     
-    /**
-     * @param string $includeArgs
-     * @throws \RuntimeException
-     */
     protected static function getPhp($includeArgs = true)
     {
         $phpFinder = new PhpExecutableFinder();
@@ -100,5 +153,60 @@ class ScriptHandler
         }
 
         return $phpPath;
+    }
+
+    protected static function getPhpArguments()
+    {
+        $arguments = array();
+
+        $phpFinder = new PhpExecutableFinder();
+        if (method_exists($phpFinder, 'findArguments')) {
+            $arguments = $phpFinder->findArguments();
+        }
+
+        if (false !== $ini = php_ini_loaded_file()) {
+            $arguments[] = '--php-ini='.$ini;
+        }
+
+        return $arguments;
+    }
+    
+    /**
+     * Returns a relative path to the directory that contains the `console` command.
+     *
+     * @param CommandEvent $event      The command event.
+     * @param string       $actionName The name of the action
+     *
+     * @return string|null The path to the console directory, null if not found.
+     */
+    protected static function getConsoleDir(CommandEvent $event, $actionName)
+    {
+        $options = static::getOptions($event);
+    
+        if (static::useNewDirectoryStructure($options)) {
+            if (!static::hasDirectory($event, 'symfony-bin-dir', $options['symfony-bin-dir'], $actionName)) {
+                return;
+            }
+    
+            return $options['symfony-bin-dir'];
+        }
+    
+        if (!static::hasDirectory($event, 'symfony-app-dir', $options['symfony-app-dir'], 'execute command')) {
+            return;
+        }
+    
+        return $options['symfony-app-dir'];
+    }
+    
+    /**
+     * Returns true if the new directory structure is used.
+     *
+     * @param array $options Composer options
+     *
+     * @return bool
+     */
+    protected static function useNewDirectoryStructure(array $options)
+    {
+        return isset($options['symfony-var-dir']) && is_dir($options['symfony-var-dir']);
     }
 }
