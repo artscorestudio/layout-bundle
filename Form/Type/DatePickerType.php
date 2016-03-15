00713ccd28b6547b10f116a326fba797b5bd6e66
Nicolas Claverie <info@artscore-studio.fr>
2016-03-15 12:50:33 +0100

    Create DatePicker Form Type (jQuery UI DatePicker)

---
+++ Form/Type/DatePickerType.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This file is part of Artscore Studio Framework Package
+ *
+ * (c) 2012-2015 Artscore Studio <info@artscore-studio.fr>
+ *
+ * This source file is subject to the MIT Licence that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace ASF\LayoutBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+/**
+ * Date Picker Form Type
+ * 
+ * @author Nicolas Claverie <info@artscore-studio.fr>
+ *
+ */
+class DatePickerType extends AbstractType
+{
+	/**
+	 * {@inheritDoc}
+	 * @see \Symfony\Component\Form\AbstractType::configureOptions()
+	 */
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults(array(
+			'date_widget' => 'single_text',
+			'attr' => array('class' => 'datepicker')
+		));
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see \Symfony\Component\Form\AbstractType::getParent()
+	 */
+	public function getParent()
+	{
+	    return DateTimeType::class;
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see \Symfony\Component\Form\FormTypeInterface::getName()
+	 */
+	public function getName()
+	{
+		return 'date_picker';
+	}
+}
